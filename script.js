7fb293070cf67b4c1ffd52f660d8f2f79d765cf8
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2023-12-24 03:40:41 -0500

    Update switch name for both pages, Remove font toggle code in js

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const darkModeSwitch = document.getElementById('darkModeSwitch');
-    const currentTheme = localStorage.getItem('theme') || 'light'; // Default to light mode
-  
-    document.documentElement.setAttribute('data-theme', currentTheme);
-  
-    function toggleDarkMode() {
-      const isDarkMode = document.documentElement.getAttribute('data-theme') === 'dark';
-  
-      if (isDarkMode) {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-        darkModeSwitch.textContent = 'Dark';
-      } else {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-        darkModeSwitch.textContent = 'Light';
-      }
+document.addEventListener("DOMContentLoaded", function () {
+  const darkModeSwitch = document.getElementById("darkModeSwitch");
+  const currentTheme = localStorage.getItem("theme") || "light";
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  function toggleDarkMode() {
+    const isDarkMode =
+      document.documentElement.getAttribute("data-theme") === "dark";
+
+    if (isDarkMode) {
+      document.documentElement.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", "light");
+      darkModeSwitch.textContent = "Dark Mode";
+    } else {
+      document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
+      darkModeSwitch.textContent = "Light Mode";
     }
-  
-    darkModeSwitch.addEventListener('click', toggleDarkMode);
-  });
-  
+  }
+
+  darkModeSwitch.addEventListener("click", toggleDarkMode);
+});
